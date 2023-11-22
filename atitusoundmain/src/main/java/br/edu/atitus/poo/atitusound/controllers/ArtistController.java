aebc685322cb04a96719414780a1cf4f57b7f90f
Otavio Augusto Lorenzatto <lorenzattoo@gmail.com>
2023-11-22 00:06:56 -0300

    primeiro commit

---
+++ atitusoundmain/src/main/java/br/edu/atitus/poo/atitusound/controllers/ArtistController.java
@@ -0,0 +1,36 @@
+package br.edu.atitus.poo.atitusound.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.poo.atitusound.dtos.ArtistDTO;
+import br.edu.atitus.poo.atitusound.entities.ArtistEntity;
+import br.edu.atitus.poo.atitusound.services.ArtistService;
+import br.edu.atitus.poo.atitusound.services.GenericService;
+
+@RestController
+@RequestMapping("/artists")
+public class ArtistController extends GenericController<ArtistEntity, ArtistDTO>{
+	//Possui uma dependência do tipo ArtistService
+	private final ArtistService service;
+	
+	public ArtistController(ArtistService service) {
+		super();
+		this.service = service;
+	}
+
+	protected ArtistEntity convertDTO2Entity(ArtistDTO dto) {
+		ArtistEntity newArtist = new ArtistEntity();
+		newArtist.setName(dto.getName());
+		newArtist.setImage(dto.getImage());
+		return newArtist;
+	}
+
+	@Override
+	protected GenericService<ArtistEntity> getService() {
+		return service;
+	}
+
+}
